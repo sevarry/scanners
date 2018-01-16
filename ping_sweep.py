72841154cd0b0b820251a1a58355fc59567cf604
sevarry <ted.eckerman@gmail.com>
2018-01-16 15:00:27 -0800

    Update ping_sweep.py

--- ping_sweep.py
+++ ping_sweep.py
@@ -3,6 +3,7 @@
 #created by Ted Eckerman, 2018
 
 import nmap
+
 nm = nmap.PortScanner()
 up_list = []
 nm.scan(hosts='192.168.1.0/24', arguments='-sP')
@@ -15,8 +16,18 @@ for host, status in hosts_list:
     up_list.append('%s'%(host))
 
 def portscan(host):
-    result = nm[host].hostname()
-    print (host + ' : ' + result)
-    
+    nm.scan(host, '22-443')
+    hostname = nm[host].hostname()
+    for proto in nm[host].all_protocols():
+        print('------------')
+        print(hostname)
+        print('------------')
+        print('Protocol: %s' % proto)
+        
+        lport = nm[host][proto].keys()
+        lport.sort()
+        for port in lport:
+            print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state'])) 
+ 
 for host in up_list:
     portscan(host)

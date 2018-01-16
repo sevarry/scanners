31df61d45535a94f62dc6707b348fbe75507be29
sevarry <ted.eckerman@gmail.com>
2018-01-16 13:54:32 -0800

    Update ping_sweep.py

--- ping_sweep.py
+++ ping_sweep.py
@@ -1,12 +1,22 @@
+#!/usr/bin/env python
+
+#created by Ted Eckerman, 2018
+
 import nmap
 nm = nmap.PortScanner()
-
-nm.scan(hosts='192.168.1.0/24', arguments='-n -sP')
+up_list = []
+nm.scan(hosts='192.168.1.0/24', arguments='-sP')
 cmd = nm.command_line()
-print ('\n' + 'Running nmap command: '+ cmd + '\n')
-print ('Hosts found: ')
 
 hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
 
 for host, status in hosts_list:
-    print(host + ' ' + status)
+    #print(host + ' ' + status)
+    up_list.append('%s'%(host))
+
+def portscan(host):
+    result = nm[host].hostname()
+    print (host + ' : ' + result)
+    
+for host in up_list:
+    portscan(host)
